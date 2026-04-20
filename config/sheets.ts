101c35ebfb21eb040830fc1893ea7eff271804a1
Dharmaraj Aparadh <aparadhdharmaraj@gmail.com>
2026-04-21 00:18:55 +0530

    chore: update to dynamic google apps script endpoint

--- config/sheets.ts
+++ config/sheets.ts
@@ -1,4 +1,4 @@
 export const SHEETS_CONFIG = {
-  // Production Apps Script Deployment URL
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycby5KA7FvsotnQCpClgjv-5PCyTYG7bbC_-1eoYJ5zVRhmZMK5rploGpTnqcpMmlqUHFTg/exec',
+  // Production Apps Script Deployment URL (Dynamic Schema Version)
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycby39Hu9etbH6Sdv_R_g9MSOV_Q9Obaq6WwHp7Mk8D_-6FNfdRSJ3lYlrKle3ZSsQF9kmg/exec',
 };
